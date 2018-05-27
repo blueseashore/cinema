24e9c2df5fadc3e258a98643c8001d06a9962cd4
kendo <455019825@qq.com>
2018-05-27 23:04:43 +0800

    test

--- public/webhook.php
+++ public/webhook.php
@@ -2,4 +2,5 @@
 /**
  * User: kendo    Date: 2018/4/24
  */
-shell_exec('cd /var/www && php gitpull.php');
+file_put_contents('/tmp/' . time() . '.log', file_get_contents('php://input'));
+//shell_exec('cd /var/www && php gitpull.php');
