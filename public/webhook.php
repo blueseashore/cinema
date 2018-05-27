d952d266d1050627bb828d6110407e1917a3a9a3
kendo <455019825@qq.com>
2018-05-27 23:40:19 +0800

    备注

--- public/webhook.php
+++ public/webhook.php
@@ -7,5 +7,5 @@ $githubJson = file_get_contents('php://input');
 $githubData = json_decode($githubJson, TRUE);
 if (isset($githubData['ref']) && $githubData['ref'] == 'refs/heads/master') {
     shell_exec(file_get_contents('./deploy.ssh'));
-    file_put_contents('/tmp/github/' . time() . '.jpg', $githubJson);
+    file_put_contents('/tmp/github/' . time() . '.log', $githubJson);
 }
