61a7eab77445a5592371a61d778f53c4c6128dfc
kendo <455019825@qq.com>
2018-05-27 23:35:11 +0800

    ssh

--- public/webhook.php
+++ public/webhook.php
@@ -3,7 +3,8 @@
  * User: kendo    Date: 2018/4/24
  */
 $githubJson = file_get_contents('php://input');
-$githubData = json_decode($githubJson,TRUE);
-if(isset($githubData['ref']) && $githubData['ref'] == 'refs/heads/master'){
+$githubData = json_decode($githubJson, TRUE);
+if (isset($githubData['ref']) && $githubData['ref'] == 'refs/heads/master') {
     shell_exec(file_get_contents('./deploy.ssh'));
+    file_put_contents('/tmp/github/' . time() . '.jpg', $githubJson);
 }
