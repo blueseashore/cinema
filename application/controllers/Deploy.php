65b7b40f5fbae71a58b3c02161193b19610bf949
kendo <455019825@qq.com>
2018-05-28 09:19:42 +0800

    deploy test

--- application/controllers/Deploy.php
+++ application/controllers/Deploy.php
@@ -5,11 +5,14 @@ class Deploy extends CI_Controller
 {
     public function index()
     {
+        echo 'start', PHP_EOL;
         $githubJson = file_get_contents('php://input');
         $githubData = json_decode($githubJson, TRUE);
         if (isset($githubData['ref']) && $githubData['ref'] == 'refs/heads/master') {
-        exec('../system/deploy.sh');
+            echo 'exec', PHP_EOL;
+            exec('../system/deploy.sh');
             file_put_contents('/tmp/github/' . time() . '.log', $githubJson);
         }
+        echo 'end', PHP_EOL;
     }
 }
