b9fc8f4164127c8c602b5312f9836faa611230a6
kendo <455019825@qq.com>
2018-06-04 15:24:01 +0800

    config

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -95,7 +95,7 @@
 <div id="header">
     <div class="wrap">
         <i class="menu_open"></i>
-        <div class="logo"><a href="http://luckendo.com/" title="模板之家"><img
+        <div class="logo"><a href="http://uckendo.com/" title="模板之家"><img
                         src="<?= $this->config->item('base_assets_url') ?>/css/logo.png"/></a></div>
         <i class="search_open"></i>
     </div>
